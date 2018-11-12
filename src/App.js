814099f16090aa24a9912af9e0e2cbc6c8bf1f83
Nazanin Delam <ndelam@myvest.com>
2018-11-11 18:04:55 -0800

    initialcommit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        txt: 'this is the state text'
+      }
+    }
+
+    update(e) {
+      this.setState({txt: e.target.value})
+    }
+
+    render() {
+        //return React.createElement('h1', null, 'Hello Universe!');
+        let txt2 = this.props.txt2;
+        return (
+          <div>
+            <input type="text"
+            onChange={this.update.bind(this)} />
+            <h1>{this.state.txt}</h1> 
+            <b>:) Wink </b> <label>{txt2}</label>
+          </div>
+        )
+    }
 }
+
+App.propTypes = {
+  txt2: PropTypes.string.isRequired,
+  cat: PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+  txt2: "this is the default txt"
+}
+
+//const App = () => <h1>Hello Universe!</h1>
 
 export default App;
